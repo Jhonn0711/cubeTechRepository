a8582e965cf861e15b35b6c08b45b23e21c16e64
joaovictor <jabelvicente@gmail.com>
2024-11-14 12:50:40 -0300

    ajustes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { get } = require('http');
 const readlineSync = require('readline-sync');
 const puppeteer = require('puppeteer');
 
-console.log('Bem vindo ao meu robozinho que brinca de scrapping');
+console.log('Bem vindo ao meu robozinho de scrapping 🤖');
     
 async function bot(){
     const browser = await puppeteer.launch({headless: true}); // .launch abre o browser; headless: true para não exibir o navegador em tela e false para exibir o processo;
@@ -19,7 +19,7 @@ async function getMoneyConvert(){ //busca a moeda desejada pelo usuário;
 
     const url = `https://www.google.com/search?client=opera-gx&q=${coinBase}+para+${convertCoin}&sourceid=opera&ie=UTF-8&oe=UTF-8s`;
 
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(url);
 
@@ -30,7 +30,6 @@ async function getMoneyConvert(){ //busca a moeda desejada pelo usuário;
     
     console.log(`O valor de 1 ${coinBase} é equivalente a ${result} ${convertCoin}`)
     
-    await browser.close();
     return;
 }
 
@@ -42,11 +41,36 @@ async function getPrecosProdutosPac(url, searchItem){
     await page.waitForSelector('#cb1-edit');
 
     await page.type('#cb1-edit', searchItem);
-    await page.click('.nav-search-btn');
 
-    return await browser.close();
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('.nav-search-btn'),
+        page.waitForSelector('a.ui-search-link__title-card'),
+    ]);
+
+    const links = await page.$$eval('a.ui-search-link__title-card', elements => elements.map(link => link.href));      
+
+
+    let index = 1;
+    var array = [];
+    for (const element of links) {
+        await page.goto(element);
+
+        await page.waitForSelector('.ui-pdp-title');
+
+        const title = await page.$eval('.ui-pdp-title', element => element.innerText);
+        const price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
+
+        const object = {title, price};
+
+        array.push(object);
+        index++;
+    }
+
+    await browser.close();
+
+    return await array;
 } 
 
-const url = 'https://www.mercadolivre.com.br/ofertas/frete-gratis#DEAL_ID=DDDD&S=MKT&V=1&T=MS&L=MKTPLACE_MULTICATEG_RTM_SEG_FRETE_NB_DESKWEB&me.audience=unknown&me.bu=3&me.bu_line=26&me.component_id=main_slider_web_ml_0&me.content_id=MS_FRETE_NB_DESKWEB&me.flow=-1&me.logic=user_journey&me.position=0&audience=unknown&bu=3&bu_line=26&component_id=main_slider_web_ml_0&content_id=MS_FRETE_NB_DESKWEB&flow=-1&logic=user_journey&position=0&c_id=/home/exhibitors-carousel/element&c_campaign=MKTPLACE_MULTICATEG_RTM_SEG_FRETE_NB_DESKWEB&c_element_order=1&c_uid=9af08759-e37c-4fa9-a9a4-936dacd31e8c';
-
-getPrecosProdutosPac(url, 'macbook');
+const url = 'https://www.mercadolivre.com.br';
+console.log(getPrecosProdutosPac(url, 'celular'));
